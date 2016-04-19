302c710ba9a4c90b5f30e434407bc0db6800efb4
Gary Schulte <gary.schulte@jivesoftware.com>
2016-04-18 17:24:30 -0700

    end of sprint exploratory aws lambda incorporating deserialization of json pojos and avro transformation

--- src/main/java/com/jivesoftware/hackday/lambdaCopy.java
+++ src/main/java/com/jivesoftware/hackday/lambdaCopy.java
@@ -1,7 +1,8 @@
 package com.jivesoftware.hackday;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URLDecoder;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,19 +12,41 @@ import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.*;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import com.jivesoftware.community.cloudalytics.event.JsonAvroCloner;
+import com.jivesoftware.community.cloudalytics.event.avro.AvroEvent;
+import com.jivesoftware.community.cloudalytics.event.jsonschema.EventDocument;
+import org.apache.avro.file.CodecFactory;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.commons.compress.utils.IOUtils;
 
 /**
  * Created by gary.schulte on 2/19/16.
+ *
+ * quick and dirty lambda function that reads cloudalytic json events and
+ * writes to s3 bucket as avro
  */
 
 public class lambdaCopy implements RequestHandler<S3Event, String> {
 
     final Pattern path = Pattern.compile("\\/?(\\d+)\\/(\\d+)\\/(\\d+)\\/(\\d+)\\/(.*)");
+    ObjectMapper mapper = new ObjectMapper();
+    ObjectMapper looseMapper = new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    AmazonS3 s3Client = new AmazonS3Client();
+
+    int numRead = 0, jsonErrors = 0, avroErrors = 0, unrecognizedPropertyErrors = 0;
 
 
     public String handleRequest(S3Event s3event, Context context) {
+        BufferedReader br = null;
+        ByteArrayOutputStream baos = null;
+        DataFileWriter<AvroEvent> s3AvroWriter = null;
+
         try {
             S3EventNotificationRecord record = s3event.getRecords().get(0);
 
@@ -33,7 +56,7 @@ public class lambdaCopy implements RequestHandler<S3Event, String> {
                     .replace('+', ' ');
             srcKey = URLDecoder.decode(srcKey, "UTF-8");
 
-            String dstBucket = "hive-" + srcBucket;
+            String dstBucket = "avro-" + srcBucket;
             Matcher m = path.matcher(srcKey);
 
             // Sanity check: validate that source and destination are different
@@ -51,21 +74,44 @@ public class lambdaCopy implements RequestHandler<S3Event, String> {
                         "/month=" + m.group(2) +
                         "/day=" + m.group(3) +
                         "/hour=" + m.group(4) +
-                        "/" +m.group(5);
+                        "/" + m.group(5);
 
                 // copying to S3 destination bucket
                 System.out.println("Copying: " + srcBucket + "/" + srcKey + " to " + dstBucket + "/" + dstKey);
 
-                // Download the image from S3 into a stream
-                AmazonS3 s3Client = new AmazonS3Client();
-                S3Object s3Object = s3Client.getObject(new GetObjectRequest(
+                S3Object s3SourceObject = s3Client.getObject(new GetObjectRequest(
                         srcBucket, srcKey));
+                br = new BufferedReader(new InputStreamReader(s3SourceObject.getObjectContent()));
 
-                // copying to S3 destination bucket
-                System.out.println("Writing to: " + dstBucket + "/" + dstKey);
-                s3Client.copyObject(srcBucket, srcKey, dstBucket, dstKey);
-                System.out.println("Successfully copied " + srcBucket + "/"
-                        + srcKey + "  to " + dstBucket + "/" + dstKey);
+
+                // target output
+                baos = new ByteArrayOutputStream();
+                s3AvroWriter = setupSnappyAvroStream(baos);
+                String doc = null;
+                while (null != (doc = br.readLine())){
+                    numRead++;
+                    try {
+                        EventDocument evtDoc = parsePOJO(doc);
+                        AvroEvent avroEvent = JsonAvroCloner.clone(evtDoc);
+                        s3AvroWriter.append(avroEvent);
+                    } catch (POJOException pjex) {
+                        jsonErrors++;
+                    } catch (IOException ioex) {
+                        avroErrors++;
+                        String errmsg = ioex.getMessage();
+                        int firstCR = errmsg.indexOf("\n");
+                        System.out.println(errmsg.substring(0, (firstCR > 0) ? firstCR : errmsg.length()));
+                    }
+                }
+
+                writeToS3(baos, dstBucket, dstKey);
+                System.out.println(String.format("%s, successfully transformed %s/%s to %s/%s"
+                        , new Date()
+                        , srcBucket
+                        , srcKey
+                        , dstBucket
+                        , dstKey));
+
                 return "Ok";
             } else {
                 System.out.println("failed to create target key from srcBucket: " + srcBucket + " and srcKey: "
@@ -75,6 +121,61 @@ public class lambdaCopy implements RequestHandler<S3Event, String> {
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            System.out.println(String.format("Processed %s docs, %s jackson errors, %s unrecognized property errors, %s avro errors"
+                    , numRead
+                    , jsonErrors
+                    , unrecognizedPropertyErrors
+                    , avroErrors));
+            IOUtils.closeQuietly(br);
+            IOUtils.closeQuietly(baos);
+            IOUtils.closeQuietly(s3AvroWriter);
+        }
+    }
+
+
+    EventDocument parsePOJO(String doc) throws POJOException {
+
+        EventDocument jsonDoc = null;
+        try {
+            try {
+                jsonDoc = mapper.readValue(doc, EventDocument.class);
+            } catch (UnrecognizedPropertyException upex) {
+                System.err.println(String.format("JACKSON at %s for \nunmapped property %s at %s ", new Date(), upex.getUnrecognizedPropertyName(), upex.getPathReference()));
+                //TODO: stats in lambda context
+                unrecognizedPropertyErrors++;
+                //upex.printStackTrace();
+                jsonDoc = looseMapper.readValue(doc, EventDocument.class);
+            }
+            return jsonDoc;
+        } catch (IOException ioex) {
+            throw new POJOException(ioex);
+        }
+    }
+
+    DataFileWriter<AvroEvent> setupSnappyAvroStream(ByteArrayOutputStream baos) throws IOException {
+        DatumWriter<AvroEvent> writer = new SpecificDatumWriter<>(AvroEvent.class);
+        DataFileWriter<AvroEvent> avroOut = new DataFileWriter<>(writer)
+                .setCodec(CodecFactory.snappyCodec())
+                .create(AvroEvent.getClassSchema(), baos);
+        return avroOut;
+    }
+
+
+    void writeToS3(ByteArrayOutputStream baos, String dstBucket, String dstKey) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        ObjectMetadata om = new ObjectMetadata();
+        om.setContentType("avro/binary");
+        om.setContentLength(baos.size());
+
+        PutObjectRequest putReq = new PutObjectRequest(dstBucket, dstKey, bais, om);
+        PutObjectResult s3TargetResult = s3Client.putObject(putReq);
+
+    }
+
+    public static class POJOException extends Exception{
+        POJOException(IOException ioex) {
+            this.initCause(ioex);
         }
     }
 }
